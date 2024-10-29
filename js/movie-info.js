0b546a255eadae085e2f7d567ec8157504a00d59
csc372 <141356425+csc372@users.noreply.github.com>
2024-10-28 20:52:30 -0400

    Add files via upload

---
+++ js/movie-info.js
@@ -0,0 +1,22 @@
+/* TO DO:
+
+    - Create an array to hold the title of your favorite movie, URL for movie poster, your rating, and synopsis.
+
+    - Select the movie image element.
+    - Set its link to the movie poster link from the array. 
+    - Set its alt text to the movie title from the array.
+
+    - Select the movie name element.
+    - Set its text to the movie title from the array.
+
+    - Select the movie description element.
+    - Set its text to the movie synopsis from the array.
+
+    - Select the movie rating element.
+    - Create a variable that will hold the filled and empty stars. 
+    - Loop to generate star symbols based on the rating:
+        - If current counter is less than your rating, then add "★".
+        - Otherwise, add "☆".
+    - Set the movie rating element's text to display the generated stars. 
+
+*/
---
+++ js/trending-movies.js
@@ -0,0 +1,37 @@
+/* TO DO:
+
+	- Create an array to hold the titles of your favorite movie *and* your partners' favorite movies.
+
+	- Create an array with URLs for each movie poster. 
+	  Note: Search Google Images for the movie title with "wiki" (i.e., "Mean Girls wiki") to find the official poster on Wikipedia.
+	
+	- Create an array with links to the HTML files for each movie page.
+	  Note: Name each HTML file after the movie title (e.g., mean-girls.html). 
+            Make sure your partners’ movie pages are live before adding links to their favorites.
+
+	- Create an array to store each movie's rating (1 to 5). Try a different rating for each movie!
+
+*/
+
+
+/* TO DO:
+	
+	- Select ALL movie card elements in the index.html file and store in a variable.
+
+	- Loop through each card to update its content with movie information:
+	  - Select the current movie image element. 
+	  - Set its link to the movie poster link from the array. 
+	  - Set its alt text to the movie title from the array. 
+
+	  - Select the current movie link element.
+	  - Set its link to the HTML page for that movie from the array. 
+	  - Set its text to the movie title from the array.
+
+	  - Select the current movie rating element.
+	  - Create a variable that will hold the filled and empty stars. 
+	  - Loop to generate star symbols based on the rating:
+	    - If current counter is less than the rating, then add "★".
+	    - Otherwise, add "☆".
+	  - Set the movie rating element's text to display the generated stars. 
+
+*/
